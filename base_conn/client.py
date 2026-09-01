1dd42f73d45afde8b386031aee741f8b5484fe01
Mari <mchagas@gmail.com>
2026-09-01 20:23:19 -0300

    chore: update server and client

--- base_conn/client.py
+++ base_conn/client.py
@@ -5,3 +5,4 @@ socket_teste = socket.socket()
 socket_teste.connect(("localhost", 5000))
 
 socket_teste.send("oiii".encode())
+print("Message sent to server!")
--- base_conn/servidor.py
+++ base_conn/servidor.py
@@ -6,10 +6,15 @@ socket_teste = socket.socket()
 socket_teste.bind(("localhost", 5000)) # disponível em localhost na porta 5000 
 
 socket_teste.listen() # aguardando conexao
-    
+print("Listening...")
+
 conn, address = socket_teste.accept() 
+print("Client connected!")
 
-msg = conn.recv(1024) # mensagem pode ser de até 1024 bytes
+msg = conn.recv(1024).decode()
+print("Message received: ", msg)
 
-print(msg)
+conn.close()
+socket_teste.close()
 
+print("Server is closed.")
